ab9ff3b148f04d586991d9973fbf786d0a905c2e
nahn945 <yukikobayashie97@gmail.com>
2026-01-04 01:11:16 +0900

    f

---
+++ MultiverseShooting/Assets/Scripts/PlayerScripts/PlayerMove.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    private Vector2 pos;
+    private float speed = 0.01f;
+    
+    private Vector2 dir;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = new Vector2(0, 0);
+        dir = new Vector2(0, 0);
+        transform.position = pos;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = new Vector2(0, 0);
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            dir.y = 1;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            dir.y = -1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            dir.x = -1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            dir.x = 1;
+        }
+
+        dir.Normalize();
+
+        pos += dir * speed;
+
+        rb.MovePosition(pos);
+    }
+}
