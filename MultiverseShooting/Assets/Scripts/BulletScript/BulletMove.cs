274fecaec7031a4f0247e7f32e8e1705ce068fb1
nahn945 <yukikobayashie97@gmail.com>
2026-01-11 22:38:52 +0900

    feat: 敵の弾幕のパターンを作成
    敵の弾幕パターンを7つ作成。
    ・単発
    ・円形弾幕
    ・Nway
    ・回転
    ・ランダム
    ・内側に進む
    ・交差

--- MultiverseShooting/Assets/Scripts/BulletScript/BulletMove.cs
+++ MultiverseShooting/Assets/Scripts/BulletScript/BulletMove.cs
@@ -1,23 +1,72 @@
+using System.Collections;
 using UnityEngine;
 
 public class BulletMove : MonoBehaviour
 {
     public float speed = 15.0f;
     public float limitY = 5.0f;
+    public float angle = 90f;
+
+    public bool isHoming = false;
+    public float homingTime = 1.0f;
+
+    public bool isCross = false;
+
+    public float crossAngle = 90f;
 
     private Rigidbody2D rb;
+    private Vector3 dir;
+
+    private GameObject player;
+
+    private bool hasCrossed = false;
 
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
+
         rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.up * speed;
+
+        if (isHoming)
+        {
+            Vector3 targetDir = player.transform.position - this.gameObject.transform.position;
+            targetDir.Normalize();
+            rb.velocity = targetDir * speed;
+        }
+        else
+        {
+            dir.x = Mathf.Cos(angle * Mathf.Deg2Rad);
+            dir.y = Mathf.Sin(angle * Mathf.Deg2Rad);
+            rb.velocity = dir * speed;
+        }
+
     }
 
     void Update()
     {
+        if (isCross && !hasCrossed)
+        {
+            StartCoroutine(makeCross());
+        }
+
         if (Mathf.Abs(transform.position.y) >= limitY)
         {
             Destroy(gameObject);
         }
+    }
+
+    IEnumerator makeCross()
+    {
+        yield return new WaitForSeconds(1);
+
+        Vector3 newDir;
+        newDir.x = Mathf.Cos((angle + crossAngle) * Mathf.Deg2Rad);
+        newDir.y = Mathf.Sin((angle + crossAngle) * Mathf.Deg2Rad);
+        newDir.z = 0;
+        newDir.Normalize();
+
+        rb.velocity = newDir * speed;
+
+        hasCrossed = true;
     }
 }
