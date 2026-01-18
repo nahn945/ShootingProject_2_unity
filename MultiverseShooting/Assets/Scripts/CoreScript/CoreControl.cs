5cc9bb1f1cf8696e86795f4fa0014c7b6078394f
nahn945 <yukikobayashie97@gmail.com>
2026-01-18 16:11:20 +0900

    feat: プレイヤーの残機の実装
    プレイヤーの残機をHPという形で実装
    ダメージを受けた際にはHPが減少し、一定時間無敵時間が発生する
    
    HPが0の際にはゲームオーバー画面を表示し、操作を一時停止する。
    ただし、Time.timeScale = 0fにする形で止めているので、今後の実装次第では停止しない処理が現れる可能性あり。

--- MultiverseShooting/Assets/Scripts/CoreScript/CoreControl.cs
+++ MultiverseShooting/Assets/Scripts/CoreScript/CoreControl.cs
@@ -6,14 +6,23 @@ public class CoreControl : MonoBehaviour
 {
     public int HP = 10;
 
+    public readonly float invincibleSec = 1.0f;
+    private float timer = 0;
+    private bool isTimerMoving = false;
+
     private bool isDead = false;
 
     private GameObject entity;
+
+    private GameSceneUIController UI;
 
     // Start is called before the first frame update
     void Start()
     {
         entity = GetComponent<GameObject>();
+        timer = invincibleSec;
+
+        UI = GameObject.FindWithTag("UIManager").GetComponent<GameSceneUIController>();
     }
 
     // Update is called once per frame
@@ -23,12 +32,29 @@ public class CoreControl : MonoBehaviour
 
         if (isDead)
         {
-            Debug.Log("Dead");
+            UI.DisplayGameOver();
+        }
+
+        if (isTimerMoving)
+        {
+            if (timer <= invincibleSec)
+            {
+                timer += Time.deltaTime;
+            }
         }
     }
 
     public void DamageHP(int value)
     {
-        HP -= value;
+        
+        if (timer >= invincibleSec)
+        {
+            Debug.Log("hit");
+            HP -= value;
+            timer = 0f;
+        }
+
+        isTimerMoving = true;
+
     }
 }
